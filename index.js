cecfac60ec2fca4876e533272cadc408a43ea417
Pier Fumagalli <pier@usrz.com>
2020-07-20 17:39:30 +0200

    Javascript wrapper

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const os = require('os')
+const native = require(`./xlib/statvfs-${os.type()}-${os.arch()}.node`.toLowerCase())
+
+module.exports = function statvfs(path) {
+  return new Promise((resolve, reject) => {
+    if (! path) throw new TypeError('No path specified')
+    if (typeof path !== 'string') throw new TypeError('Invalid type for path')
+
+    native(path, (error, total, avail, free) => {
+      if (error) return reject(error)
+      return resolve({
+        total: total,
+        available: avail,
+        free: free,
+      })
+    })
+  })
+}
---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const statvfs = require('.')
+
+async function main(path, unit = 'b') {
+  let divider = 1
+  switch (unit.toLowerCase()) {
+    case 'b': divider = 1; break
+    case 'k': divider = 1024; break
+    case 'm': divider = 1024 * 1024; break
+    case 'g': divider = 1024 * 1024 * 1024; break
+    default: throw new TypeError(`Invalid unit ${unit}`)
+  }
+
+  const results = await(statvfs(path))
+  for (const key in results) {
+    if (key === 'usage') console.log('usage (%)', results[key])
+    else console.log(key, Math.round(results[key] / divider))
+  }
+}
+
+main(...process.argv.slice(2)).catch((error) => console.error(error))
