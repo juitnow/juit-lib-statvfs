32852ebca016867b5b3dd6d9771ef45ffb4862a1
Pier Fumagalli <pier@usrz.com>
2020-07-20 18:36:17 +0200

    Test native lib

--- index.js
+++ index.js
@@ -4,17 +4,28 @@ const os = require('os')
 const native = require(`./lib/statvfs-${os.type()}-${os.arch()}.node`.toLowerCase())
 
 module.exports = function statvfs(path) {
-  return new Promise((resolve, reject) => {
+  const promise = new Promise((resolve, reject) => {
     if (! path) throw new TypeError('No path specified')
     if (typeof path !== 'string') throw new TypeError('Invalid type for path')
 
+    console.log('FOO')
     native(path, (error, total, avail, free) => {
-      if (error) return reject(error)
-      resolve({
-        total: total,
-        available: avail,
-        free: free,
-      })
+      console.log('BAR1', error, total, avail, free, promise)
+
+      if (error) {
+        reject(error)
+      } else {
+        resolve({
+          total: total,
+          available: avail,
+          free: free,
+        })
+      }
+
+      console.log('BAR2', error, total, avail, free, promise)
+
     })
   })
+
+  return promise
 }
--- test/statvfs.test.js
+++ test/statvfs.test.js
@@ -3,58 +3,106 @@
 /* eslint no-invalid-this: off */
 const { expect } = require('chai')
 const mockery = require('mockery')
+const os = require('os')
 
 describe('StatVFS interface', () => {
-  let statvfs = null, args = [], error = null
+  // describe('mocked native', () => {
+  //   let statvfs = null, args = [], error = null
 
-  before(() => {
-    mockery.registerMock('./lib/statvfs-darwin-x64.node', (path, callback) => {
-      expect(path).to.eql('/the-path')
-      if (error) throw error
-      callback(...args)
+  //   before(() => {
+  //     const binary = `./lib/statvfs-${os.type()}-${os.arch()}.node`.toLowerCase()
+  //     mockery.registerMock(binary, (path, callback) => {
+  //       expect(path).to.eql('/the-path')
+  //       if (error) throw error
+  //       setTimeout(() => callback(...args), 100)
+  //     })
+
+  //     mockery.enable({
+  //       warnOnReplace: false,
+  //       warnOnUnregistered: false,
+  //     })
+  //   })
+
+  //   after(() => {
+  //     mockery.deregisterAll()
+  //     mockery.disable()
+  //   })
+
+  //   it('should load the module', function() {
+  //     this.slow(1000)
+
+  //     const index = require('..')
+  //     expect(index).to.be.a('function')
+  //     statvfs = index
+  //   })
+
+  //   it('should invoke successfully', async function() {
+  //     if (!statvfs) return this.skip()
+
+  //     args = [ undefined, 1, 2, 3 ], error = null
+  //     expect(await statvfs('/the-path')).to.eql({
+  //       total: 1,
+  //       available: 2,
+  //       free: 3,
+  //     })
+  //   })
+
+  //   it('should reject without a path', async function() {
+  //     if (!statvfs) return this.skip()
+
+  //     await expect(statvfs()).to.be.rejectedWith(TypeError, 'No path specified')
+  //     await expect(statvfs(true)).to.be.rejectedWith(TypeError, 'Invalid type for path')
+  //   })
+
+  //   it('should reject fail on callback error', async function() {
+  //     if (!statvfs) return this.skip()
+
+  //     args = [ new TypeError('Foo Bar Baz') ], error = null
+  //     await expect(statvfs('/the-path')).to.be.rejectedWith(TypeError, 'Foo Bar Baz')
+  //   })
+
+  //   it('should reject when native throws', async function() {
+  //     if (!statvfs) return this.skip()
+
+  //     args = [], error = new TypeError('Foo Bar Baz')
+  //     await expect(statvfs('/the-path')).to.be.rejectedWith(TypeError, 'Foo Bar Baz')
+  //   })
+  // })
+
+  describe('real native', () => {
+    before(() => {
+      delete require.cache[require.resolve('..')]
     })
 
-    mockery.enable({
-      warnOnReplace: false,
-      warnOnUnregistered: false,
+    it('should invoke successfully', async () => {
+      // this.slow(10000)
+
+      const statvfs = require('../index.js')
+      console.log('REQUIRED')
+
+      const promise = statvfs('/') //new Promise((resolve, reject) => {
+      // setTimeout(() => resolve({}), 1000)
+      // })// statvfs('/')
+      console.log('PROMISE', promise)
+      // .then((result) => {
+      //   console.log('YO')
+      //   return result
+      // })
+      promise.then(() => {
+        console.log('RESOLVED')
+      })
+
+      const result = await promise
+
+      console.log('awaited')
+
+      expect(result).to.be.an('object')
+      expect(result).to.have.keys([ 'total', 'available', 'free' ])
+      expect(result.total).to.be.a('number').greaterThan(100)
+      expect(result.available).to.be.a('number').greaterThan(100)
+      expect(result.free).to.be.a('number').greaterThan(100)
+
+      // done()
     })
-  })
-
-  it('should load the module', () => {
-    const index = require('..')
-    expect(index).to.be.a('function')
-    statvfs = index
-  })
-
-  it('should invoke successfully', async () => {
-    if (!statvfs) return this.skip()
-
-    args = [ undefined, 1, 2, 3 ], error = null
-    expect(await statvfs('/the-path')).to.eql({
-      total: 1,
-      available: 2,
-      free: 3,
-    })
-  })
-
-  it('should reject without a path', function() {
-    if (!statvfs) return this.skip()
-
-    expect(statvfs()).to.be.rejectedWith(TypeError, 'No path specified')
-    expect(statvfs(true)).to.be.rejectedWith(TypeError, 'Invalid type for path')
-  })
-
-  it('should reject fail on callback error', async () => {
-    if (!statvfs) return this.skip()
-
-    args = [ new TypeError('Foo Bar Baz') ], error = null
-    expect(statvfs('/the-path')).to.be.rejectedWith(TypeError, 'Foo Bar Baz')
-  })
-
-  it('should reject when native throws', async () => {
-    if (!statvfs) return this.skip()
-
-    args = [], error = new TypeError('Foo Bar Baz')
-    expect(statvfs('/the-path')).to.be.rejectedWith(TypeError, 'Foo Bar Baz')
   })
 })
