231bc65d9f504016217a2e2bc79990c2d996deb9
Pier Fumagalli <pier@usrz.com>
2020-07-27 15:53:11 +0200

    Use the NAPI

--- native/statvfs.cc
+++ native/statvfs.cc
@@ -1,150 +1,79 @@
-#include <node.h>
-#include <uv.h>
-
 #include <sys/statvfs.h>
-#include <errno.h>
+#include <napi.h>
+#include "errno-error.h"
 
+using namespace Napi;
+using namespace Errno;
 
-namespace node_statvfs {
-    using node::ErrnoException;
+namespace StatVFS {
 
-    using v8::Exception;
-    using v8::Function;
-    using v8::FunctionCallbackInfo;
-    using v8::HandleScope;
-    using v8::Isolate;
-    using v8::Local;
-    using v8::NewStringType;
-    using v8::Null;
-    using v8::Number;
-    using v8::Object;
-    using v8::Persistent;
-    using v8::String;
-    using v8::Value;
+  class StatVFSAsyncWorker : public AsyncWorker {
 
-    /* Define our structure for asynchronous processing */
-    struct AsyncData {
-        Persistent<Function> callback; // the callback invoked after statvfs
-        const char * path; // the path to check for free space
-        int64_t total; // total number of bytes in the filesystem
-        int64_t avail; // available number of bytes in the filesystem
-        int64_t free; // free (including restricted) bytes in the filesystem
-        int error; // when non-zero this is errno from statvfs
-    };
+    public:
+      StatVFSAsyncWorker(String& path, Function& callback)
+      : AsyncWorker(callback) {
+        std::string str = path.Utf8Value();
 
-    static void StartAsync(const FunctionCallbackInfo<Value>& args);
-    static void RunAsync(uv_work_t * request);
-    static void FinishAsync(uv_work_t *request, int status);
+        std::copy(str.begin(), str.end(), this->path);
+        this->path[str.size()] = '\0';
 
-    /* Start asynchronous processing: check arguments and schedule job */
-    static void StartAsync(const FunctionCallbackInfo<Value>& args) {
-        Isolate* isolate = args.GetIsolate();
+        std::memset((void *) &stat, 0, sizeof(struct statvfs));
+      };
 
-        // Two arguments: path and callback
-        if (args.Length() < 2) {
-            isolate->ThrowException(Exception::TypeError(
-                String::NewFromUtf8(isolate,
-                                    "Two arguments (path and callback) required",
-                                    NewStringType::kNormal).ToLocalChecked()));
-            return;
-        }
+      void Execute() {
+        if (statvfs(path, &stat)) error = errno;
+      };
 
-        if (!args[0]->IsString()) {
-            isolate->ThrowException(Exception::TypeError(
-                String::NewFromUtf8(isolate,
-                                    "Wrong type for first argument (path)",
-                                    NewStringType::kNormal).ToLocalChecked()));
-            return;
-        }
+      void OnOK() {
+        class Env env = Env();
 
-        if (!args[1]->IsFunction()) {
-            isolate->ThrowException(Exception::TypeError(
-                String::NewFromUtf8(isolate,
-                                    "Wrong type for second argument (callback)",
-                                    NewStringType::kNormal).ToLocalChecked()));
-            return;
-        }
-
-        // Setup our "arguments" structure
-        AsyncData * data = new AsyncData();
-
-        String::Utf8Value path(isolate, args[0]);
-        char *path_copy = (char *) calloc(path.length() + 1, sizeof(char));
-        strcpy(path_copy, *path);
-        data->path = path_copy;
-
-        Local<Function> callback = Local<Function>::Cast(args[1]);
-        data->callback.Reset(isolate, callback);
-
-        data->error = 0;
-        data->total = -1;
-        data->avail = -1;
-        data->free = -1;
-
-        // Queue up our work request for asynchronous processing
-        uv_work_t * request = new uv_work_t();
-        request->data = data;
-        uv_queue_work(uv_default_loop(), request, RunAsync, FinishAsync);
-
-        // Always return "undefined"
-        args.GetReturnValue().Set(Undefined(isolate));
-    }
-
-    /* Asynchronously run statvfs and set total and available bytes */
-    static void RunAsync(uv_work_t * request) {
-        AsyncData * data = (AsyncData *) request->data;
-        struct statvfs buf;
-
-        int error = statvfs(data->path, &buf);
-
-        if (error != 0) {
-            data->error = errno;
-            data->total = -1;
-            data->avail = -1;
-            data->free = -1;
+        if (error) {
+          Callback().Call({ ErrnoError(env, error, "statvfs", path ).Value() });
         } else {
-            data->error = 0;
-            data->total = buf.f_blocks * buf.f_frsize;
-            data->avail = buf.f_bavail * buf.f_frsize;
-            data->free = buf.f_bfree * buf.f_frsize;
+          Object result = Object::New(env);
+
+          result.Set("f_bsize", (double) stat.f_bsize);     /* File system block size */
+          result.Set("f_frsize", (double) stat.f_frsize);   /* Fundamental file system block size */
+          result.Set("f_blocks", (double) stat.f_blocks);   /* Blocks on FS in units of f_frsize */
+          result.Set("f_bfree", (double) stat.f_bfree);     /* Free blocks */
+          result.Set("f_bavail", (double) stat.f_bavail);   /* Blocks available to non-root */
+          result.Set("f_files", (double) stat.f_files);     /* Total inodes */
+          result.Set("f_ffree", (double) stat.f_ffree);     /* Free inodes */
+          result.Set("f_favail", (double) stat.f_favail);   /* Free inodes for non-root */
+          result.Set("f_fsid", (double) stat.f_fsid);       /* Filesystem ID */
+          result.Set("f_flag", (double) stat.f_flag);       /* Bit mask of values */
+          result.Set("f_namemax", (double) stat.f_namemax); /* Max file name length */
+
+          Callback().Call({ Env().Undefined(), result });
         }
-    }
+      };
 
-    /* Finish up our asynchronous job invoking the callback */
-    static void FinishAsync(uv_work_t *request, int status) {
-        AsyncData * data = (AsyncData *) request->data;
-        Isolate *isolate = Isolate::GetCurrent();
-        HandleScope handleScope(isolate);
+    private:
+      char path[PATH_MAX];
+      struct statvfs stat;
+      int error = 0;
+};
 
-        const unsigned int argc = data->error ? 1 : 4;
-        Local<Value> argv[argc];
+  Value startStatVFS(const CallbackInfo& info) {
+    Env env = info.Env();
 
-        if (data->error) {
-            argv[0] = ErrnoException(isolate, data->error, "statvfs", NULL, data->path);
-        } else {
-            argv[0] = Undefined(isolate);
-            argv[1] = Number::New(isolate, data->total);
-            argv[2] = Number::New(isolate, data->avail);
-            argv[3] = Number::New(isolate, data->free);
-        }
+    if (info.Length() != 2) throw TypeError::New(env, "Two arguments required: path, callback");
+    if (! info[0].IsString()) throw TypeError::New(env, "Path must be a string");
+    if (! info[1].IsFunction()) throw TypeError::New(env, "Callback must be a function");
 
-        (void) Local<Function>::New(isolate, data->callback)->Call(
-            isolate->GetCurrentContext(),
-            Null(isolate),
-            argc,
-            argv
-        );
+    String path = info[0].As<String>();
+    Function callback = info[1].As<Function>();
 
-        // Cleanup
-        free((void *) data->path);
-        data->callback.Reset();
-        delete data;
-        delete request;
-    }
+    StatVFSAsyncWorker* worker = new StatVFSAsyncWorker(path, callback);
+    worker->Queue();
 
-    void Init(Local<Object> exports, Local<Object> module) {
-        NODE_SET_METHOD(module, "exports", StartAsync);
-    }
+    return env.Undefined();
+  };
 
-    NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+  Object Init(Env env, Object exports) {
+    exports["statvfs"] = Function::New(env, startStatVFS, std::string("statvfs"));
+    return exports;
+  };
+
+  NODE_API_MODULE(addon, Init)
 } // namespace statvfs
