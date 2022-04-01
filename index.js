942ccb9e98cfb859e5e01a64c8c482e5454cd0e6
Pier Fumagalli <pier@usrz.com>
2022-04-01 20:13:11 +0200

    Promise *and* callback style

--- index.js
+++ index.js
@@ -4,20 +4,31 @@ const os = require('os')
 const platform = `${os.type()}-${os.arch()}`.toLowerCase()
 const native = require(`./native/${platform}/statvfs.node`)
 
-module.exports = function statvfs(path) {
-  return new Promise((resolve, reject) => {
-    if (! path) throw new TypeError('No path specified')
-    if (typeof path !== 'string') throw new TypeError('Invalid type for path')
+module.exports = function statvfs(path, cb) {
+  if (! cb) {
+    return new Promise((resolve, reject) => {
+      try {
+        statvfs(path, (error, result) => {
+          if (error) reject(error)
+          else resolve(result)
+        })
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 
-    native.statvfs(path, (error, stat) => {
-      if (error) return reject(error)
-      const frsize = stat.f_frsize
+  if (! path) throw new TypeError('No path specified')
+  if (typeof path !== 'string') throw new TypeError('Invalid type for path')
 
-      resolve({
-        total: stat.f_blocks * frsize,
-        available: stat.f_bavail * frsize,
-        free: stat.f_bfree * frsize,
-      })
+  native.statvfs(path, (error, stat) => {
+    if (error) return cb(error)
+    const frsize = stat.f_frsize
+
+    cb(undefined, {
+      total: stat.f_blocks * frsize,
+      available: stat.f_bavail * frsize,
+      free: stat.f_bfree * frsize,
     })
   })
 }
