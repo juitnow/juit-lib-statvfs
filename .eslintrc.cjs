7f5c444f26e14cf417d01110b1d473eabad9da62
Pier Fumagalli <pier@usrz.com>
2023-02-08 19:26:45 +0100

    Ignore dependencies for tests

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,4 +5,10 @@ module.exports = {
   extends: [
     'plugin:@plugjs/typescript',
   ],
+  overrides: [ {
+    files: [ 'test/**' ],
+    rules: {
+      'import/no-extraneous-dependencies': 'off',
+    },
+  } ],
 }
