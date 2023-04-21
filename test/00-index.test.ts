ba77d0146e6187605408c854f4b218a0f3df7008
Pier Fumagalli <pier@usrz.com>
2023-04-21 12:42:41 +0200

    New build

--- test/00-index.test.ts
+++ test/00-index.test.ts
@@ -5,47 +5,33 @@ import { statvfs } from '../src/index'
 describe('StatVFS', () => {
   it('should run its native component', async () => {
     const stats = await statvfs('/')
-    expect(Object.keys(stats)).toEqual(jasmine.arrayContaining([
-      'blocks_total',
-      'blocks_free',
-      'blocks_available',
-      'blocks_used',
-      'bytes_total',
-      'bytes_free',
-      'bytes_available',
-      'bytes_used',
-      'files_total',
-      'files_free',
-      'files_used',
-      'percentage_used',
-      'percentage_free',
-    ]))
+    expect(stats).toEqual({
+      blocks_total: expect.toBeA('bigint'),
+      blocks_free: expect.toBeA('bigint'),
+      blocks_available: expect.toBeA('bigint'),
+      blocks_used: expect.toBeA('bigint'),
+      bytes_total: expect.toBeA('bigint'),
+      bytes_free: expect.toBeA('bigint'),
+      bytes_available: expect.toBeA('bigint'),
+      bytes_used: expect.toBeA('bigint'),
+      files_total: expect.toBeA('bigint'),
+      files_free: expect.toBeA('bigint'),
+      files_used: expect.toBeA('bigint'),
+      percentage_used: expect.toBeA('number'),
+      percentage_free: expect.toBeA('number'),
+    })
 
-    expect(typeof stats.blocks_total).toBe('bigint')
-    expect(typeof stats.blocks_free).toBe('bigint')
-    expect(typeof stats.blocks_available).toBe('bigint')
-    expect(typeof stats.blocks_used).toBe('bigint')
-    expect(typeof stats.bytes_total).toBe('bigint')
-    expect(typeof stats.bytes_free).toBe('bigint')
-    expect(typeof stats.bytes_available).toBe('bigint')
-    expect(typeof stats.bytes_used).toBe('bigint')
-    expect(typeof stats.files_total).toBe('bigint')
-    expect(typeof stats.files_free).toBe('bigint')
-    expect(typeof stats.files_used).toBe('bigint')
-    expect(typeof stats.percentage_used).toBe('number')
-    expect(typeof stats.percentage_free).toBe('number')
-
-    expect(stats.blocks_total).toBeGreaterThan(0)
-    expect(stats.blocks_free).toBeGreaterThan(0)
-    expect(stats.blocks_available).toBeGreaterThan(0)
-    expect(stats.blocks_used).toBeGreaterThan(0)
-    expect(stats.bytes_total).toBeGreaterThan(0)
-    expect(stats.bytes_free).toBeGreaterThan(0)
-    expect(stats.bytes_available).toBeGreaterThan(0)
-    expect(stats.bytes_used).toBeGreaterThan(0)
-    expect(stats.files_total).toBeGreaterThan(0)
-    expect(stats.files_free).toBeGreaterThan(0)
-    expect(stats.files_used).toBeGreaterThan(0)
+    expect(stats.blocks_total).toBeGreaterThan(0n)
+    expect(stats.blocks_free).toBeGreaterThan(0n)
+    expect(stats.blocks_available).toBeGreaterThan(0n)
+    expect(stats.blocks_used).toBeGreaterThan(0n)
+    expect(stats.bytes_total).toBeGreaterThan(0n)
+    expect(stats.bytes_free).toBeGreaterThan(0n)
+    expect(stats.bytes_available).toBeGreaterThan(0n)
+    expect(stats.bytes_used).toBeGreaterThan(0n)
+    expect(stats.files_total).toBeGreaterThan(0n)
+    expect(stats.files_free).toBeGreaterThan(0n)
+    expect(stats.files_used).toBeGreaterThan(0n)
     expect(stats.percentage_used).toBeGreaterThan(0)
     expect(stats.percentage_free).toBeGreaterThan(0)
 
